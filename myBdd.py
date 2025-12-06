d32a685c2fe60a71317caaea18fb42d95789c9f5
igolovachenko@yandex.ru <igolovachenko@yandex.ru>
2025-12-06 20:09:41 +0300

    itmo dm hw3 task9

--- myBdd.py
+++ myBdd.py
@@ -76,11 +76,25 @@ class BDDNode:
             label = f"X{self.var}"
         return label
 
+    def displayGraph(self):
+        """ displays the graph """
+        img = self.getPng()
+        display(img)
+
+    def getPng(self):
+        """ returns the png image of the graph """
+        self.graph = pydot.Dot(graph_type="digraph")
+        # visited set for dfs to keep track of visited nodes
+        visited = set()
+        # call dfs to get the nodes in post order
+        BDD.dfs(self.graph, self, visited)
+        return Image(self.graph.create_png())
+
 
 class BDD:
     """class for BDD"""
 
-    def __init__(self, exp: boolfunc.Expression, ordering: list) -> None:
+    def __init__(self, exp: boolfunc.Expression, ordering: list, min_optimize_idx: int=0) -> None:
         """ constructor for BDD class """
         # store the number of variables
         self.exp = exp
@@ -93,21 +107,30 @@ class BDD:
         self.NODES[self.BDDNODEZERO.getKey()] = self.BDDNODEZERO
         self.BDDNODEONE = BDDNode.getNodeOne(exp.numVars)
         self.NODES[self.BDDNODEONE.getKey()] = self.BDDNODEONE
+        self.graph = None
         # build the bdd
-        self.node = self.buildBDD()
+        self.node = self.buildBDD(min_optimize_idx=min_optimize_idx)
 
     def displayGraph(self):
         """ displays the graph """
         img = self.getPng()
         display(img)
+    
+    def getNodeCount(self):
+        """ returns node count in the graph """
+        self.graph = pydot.Dot(graph_type="digraph")
+        visited = set()
+        BDD.dfs(self.graph, self.node, visited)
+        return len(self.graph.get_nodes())
 
     def getPng(self):
         """ returns the png image of the graph """
-        self.graph = pydot.Dot(graph_type="digraph")
-        # visited set for dfs to keep track of visited nodes
-        visited = set()
-        # call dfs to get the nodes in post order
-        BDD.dfs(self.graph, self.node, visited)
+        if self.graph is None:
+            self.graph = pydot.Dot(graph_type="digraph")
+            # visited set for dfs to keep track of visited nodes
+            visited = set()
+            # call dfs to get the nodes in post order
+            BDD.dfs(self.graph, self.node, visited)
         return Image(self.graph.create_png())
 
     def getExpression(self):
@@ -127,13 +150,16 @@ class BDD:
             visited.add(node)
             if node.var == -1:
                 graph.add_node(pydot.Node(node._getUid(),
+                                          label=node.getLabel(),
                                           style="filled", fillcolor="orange", shape='box'))
             elif node.var == -2:
                 graph.add_node(pydot.Node(node._getUid(),
+                                          label=node.getLabel(),
                                           style="filled", fillcolor="lightblue", shape='box'))
             else:
                 graph.add_node(pydot.Node(node._getUid(),
-                                          style="filled", fillcolor="green"))
+                                          label=node.getLabel(),
+                                          style="filled", fillcolor="white"))
             if node.lo is not None:
                 eLo = pydot.Edge(
                     node._getUid(), node.lo._getUid(), color='red', style='dotted')
@@ -143,9 +169,9 @@ class BDD:
                     node._getUid(), node.hi._getUid(), color='blue')
                 graph.add_edge(eHi)
 
-    def buildBDD(self):
+    def buildBDD(self, min_optimize_idx: int=0):
         """ builds the bdd """
-        return buildBDD(self.exp, self.ordering, self.NODES)
+        return buildBDD(self.exp, self.ordering, self.NODES, idx=0, min_optimize_idx=min_optimize_idx)
 
     def dfsPreorder(self):
         """ returns the dfs preorder traversal of the bdd """
@@ -163,49 +189,109 @@ class BDD:
         return _bfs(self.node, visited)
 
 
-def buildBDD(exp, ordering, cache):
+def buildBDD(exp, ordering, cache, idx=0, min_optimize_idx=0):
     """ builds the bdd """
+    print(f"buildBDD: idx={idx}, {ordering[idx:]}, len(ord)={len(ordering)}")
+    # print(f"exp={exp}")
+
     # if expression is false return the zero node
     if exp.isFalse():
-        return cache[BDDNode.BDDNODEZEROKEY]
+        if min_optimize_idx <= len(ordering):
+            return cache[BDDNode.BDDNODEZEROKEY]
+        if idx == len(ordering):
+            return BDDNode(exp, -1)
+        
     # if expression is true return the one node
     if exp.isTrue():
-        return cache[BDDNode.BDDNODEONEKEY]
+        if min_optimize_idx <= len(ordering):
+            return cache[BDDNode.BDDNODEONEKEY]
+        if idx == len(ordering):
+            return BDDNode(exp, -2)
+    
     # get the variable with the highest priority
-    for idx, var in enumerate(ordering):
-        if exp.isPresent(var):
-            # build the node for the variable
-            return bddNode(exp, ordering, cache, idx)
+    for idx2, var in enumerate(ordering[idx:]):
+        # build the node for the variable
+        return bddNode(exp, ordering, cache, idx + idx2, min_optimize_idx)
+    
     # if no variable is present throw an error
     raise ValueError("invalid ordering list")
 
 
-def bddNode(exp, ordering, cache, idx):
+def bddNode(exp, ordering, cache, idx, min_optimize_idx=0):
     """ returns the bdd node """
+    # print(f"bddNode: {idx}")
+
     # get the variable
     var = ordering[idx]
     # build the lo node
-    nodeLo = buildBDD(exp.negativeCofactor(var), ordering, cache)
+    nodeLo = buildBDD(exp.negativeCofactor(var), ordering, cache, idx + 1, min_optimize_idx)
     # build the hi node
-    nodeHi = buildBDD(exp.positiveCofactor(var), ordering, cache)
-    # Reduction rule 1
-    # is lo is hi then return lo
-    if nodeLo is nodeHi:
-        exp = nodeLo.exp
-        node = nodeLo
-    else:
-        # Reduction rule 2
-        # if the node is already present in the cache then return the node
-        key = (var, id(nodeLo), id(nodeHi))
-        try:
-            node = cache[key]
-        except KeyError:
-            # create the node if no reduction is possible
-            node = BDDNode(exp, var)
-            node.lo = nodeLo
-            node.hi = nodeHi
-            # store it in the cache
-            cache[key] = node
+    nodeHi = buildBDD(exp.positiveCofactor(var), ordering, cache, idx + 1, min_optimize_idx)
+    key = (var, id(nodeLo), id(nodeHi))
+    
+    if idx >= min_optimize_idx:
+        # print(f"optimize {var}:  >= {min_optimize_idx}")
+        # Reduction rule 1
+        # is lo is hi then return lo
+        if nodeLo is nodeHi:
+            exp = nodeLo.exp
+            node = nodeLo
+            return node
+        else:
+            # Reduction rule 2
+            # if the node is already present in the cache then return the node
+            try:
+                node = cache[key]
+                return node
+            except KeyError:
+                pass
+    # else:
+    #     print(f"don't optimize {var}")
+    
+    node = BDDNode(exp, var)
+    node.lo = nodeLo
+    node.hi = nodeHi
+    if idx >= min_optimize_idx:
+        cache[key] = node
+    return node
+
+
+def applyAnd(f, g, ordering, cache, idx=0):
+    return BDD(f.exp.andExp(g.exp.cubes), ordering)
+
+    if f.exp.isFalse() or g.exp.isFalse():
+        return cache[BDDNode.BDDNODEZEROKEY]
+    if f.exp.isTrue() and g.exp.isTrue():
+        return cache[BDDNode.BDDNODEONEKEY]
+    
+    topvar = -1
+    for idx2, var in enumerate(ordering[idx:]):
+        # print(f"{var}:  {f.exp.isPresent(var)}  {g.exp.isPresent(var)}")
+        if f.exp.isPresent(var) or g.exp.isPresent(var):
+            topvar = var
+    
+    if topvar == -1:
+        raise ValueError("invalid ordering")
+    
+    if f.var != g.var:
+        raise ValueError("diff vars: ", f.var, g.var)
+
+    f_lo = f.lo #buildBDD(f.exp.negativeCofactor(topvar), ordering, cache, idx + 1)
+    f_hi = f.hi #buildBDD(f.exp.positiveCofactor(topvar), ordering, cache, idx + 1)
+    g_lo = g.lo #buildBDD(g.exp.negativeCofactor(topvar), ordering, cache, idx + 1)
+    g_hi = g.hi #buildBDD(g.exp.positiveCofactor(topvar), ordering, cache, idx + 1)
+
+    nodeLo = applyAnd(f_lo, g_lo, ordering, cache, idx + 1)
+    nodeHi = applyAnd(f_hi, g_hi, ordering, cache, idx + 1)
+
+    if (nodeLo is nodeHi):
+        return nodeLo
+
+    node = BDDNode(boolfunc.Expression(), topvar)
+    node.lo = nodeLo
+    node.hi = nodeHi
+    key = (topvar, id(nodeLo), id(nodeHi))
+    cache[key] = node
     return node
 
 
